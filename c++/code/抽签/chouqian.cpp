bfdd321bbdd27042a7040ee14be9a8802cdd46c3
songhaorun <1963535597@qq.com>
2023-05-31 00:39:11 +0800

    添加抽签历史记录

--- c++/code/抽签/chouqian.cpp
+++ c++/code/抽签/chouqian.cpp
@@ -1,29 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-int nian=0,yue=0,ri=0,n=0;
+int nian=0,yue=0,ri=0,n[6];
 string qian;
 void chouqian(){
+    ++n[0];
     srand(time(0));
     int t=rand()%10+1;
     if(t==1){
         qian="上上签";
-        ++n;
+        ++n[1];
     }
-    else if(t<=3)
+    else if(t<=3){
         qian="上签";
-    else if(t<=7)
+        ++n[2];
+    }
+    else if(t<=7){
         qian="中签";
-    else if(t<=9)
+        ++n[3];
+    }
+    else if(t<=9){
         qian="下签";
-    else
+        ++n[4];
+    }
+    else{
         qian="下下签";
+        ++n[5];
+    }
 }
 int main(){
+    memset(n,0,sizeof(n));
     ifstream fin("data");
     if(!fin.is_open())
         cout<<"未找到数据文件，将自动创建"<<endl;
-    else
-        fin>>nian>>yue>>ri>>qian>>n;
+    else{
+        fin>>nian>>yue>>ri>>qian;
+        for(int i=0;i<=5;++i)
+            fin>>n[i];
+    }
     fin.close();
     ofstream fout("data");
     time_t now=time(0);
@@ -34,7 +47,17 @@ int main(){
         yue=tnow->tm_mon+1;
         nian=tnow->tm_year+1900;
     }
-    cout<<"你今天抽到的签为"<<qian<<endl<<"截止到今日，你共抽到上上签"<<n<<"次"<<endl;
-    fout<<nian<<" "<<yue<<" "<<ri<<endl<<qian<<endl<<n;
+    cout<<"你今天抽到的签为"<<qian<<endl;
+    cout<<"抽签统计:"<<endl;
+    cout<<"共抽"<<n[0]<<"次"<<endl;
+    cout<<"上上签"<<n[1]<<"次"<<endl;
+    cout<<"上签"<<n[2]<<"次"<<endl;
+    cout<<"中签"<<n[3]<<"次"<<endl;
+    cout<<"下签"<<n[4]<<"次"<<endl;
+    cout<<"下下签"<<n[5]<<"次"<<endl;
+    fout<<nian<<" "<<yue<<" "<<ri<<endl<<qian<<endl;
+    for(int i=0;i<=5;++i)
+        fout<<n[i]<<endl;
+    fout.close();
     return 0;
 }
